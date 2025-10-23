9ddd6480282d0f79b9052e2997f958d09bf35b12
DixuanThirteen <114277246+DixuanThirteen@users.noreply.github.com>
2025-10-23 10:50:09 +0800

    2025/10/23

---
+++ SectionOne/1-5_Retable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+  int Fahrenheit , Celsius;
+  int step , lower , upper;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  printf("华氏度和摄氏度对照表\n");
+  printf("华氏度\t摄氏度\n");
+
+  
+  for(Fahrenheit = upper ; Fahrenheit > lower ; Fahrenheit -= step){
+    Celsius = 5 * (Fahrenheit - 32) / 9 ;//C = 5 × (F - 32) / 9
+    printf("%d\t%d\n", Fahrenheit , Celsius);
+  }
+  return 0;
+}
